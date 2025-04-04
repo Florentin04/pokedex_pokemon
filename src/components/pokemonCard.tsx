79d68219ee8449c81e6df1d1e8deff0a362be737
Kaneb Florentin <Florentink.pro@gmail.com>
2025-04-04 11:07:14 +0200

    ajout du props.1

---
+++ src/components/pokemonCard.tsx
@@ -0,0 +1,9 @@
+function PokemonCard({pokemon}) {
+  return (
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="bulbasaur" /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
+export default PokemonCard;
