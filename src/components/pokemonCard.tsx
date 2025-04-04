cb306820e7c9af89721c54184cb6a2a5192eb941
Kaneb Florentin <Florentink.pro@gmail.com>
2025-04-04 11:11:32 +0200

    Résolution des conflits props.1

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,7 +1,20 @@
-function PokemonCard({pokemon}) {
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+  function PokemonCard() {
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="bulbasaur" /> : <p>???</p>}
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="pokemon" /> : <p>???</p>}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
